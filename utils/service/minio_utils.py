39e2c98d6aa7e532e6ac1dc34f68d0d3581d5eb8
Gushuchun <3090228211@qq.com>
2025-05-26 20:52:50 +0800

    修复傻逼错误

--- utils/service/minio_utils.py
+++ utils/service/minio_utils.py
@@ -9,7 +9,7 @@ from django.conf import settings
 minio_client = Minio(
     settings.MINIO_STORAGE_ENDPOINT,
     access_key=settings.MINIO_STORAGE_ACCESS_KEY,
-    secret_key=settings.MINIO_STORAGE_ACCESS_KEY,
+    secret_key=settings.MINIO_STORAGE_SECRET_KEY,
     secure=settings.MINIO_STORAGE_USE_HTTPS
 )
 
